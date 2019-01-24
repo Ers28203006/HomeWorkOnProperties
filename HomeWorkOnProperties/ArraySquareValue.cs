84e1885d9fa5891664d4f3e5e5534d526988f0b7
Султанбаев Ерсын Балабулатович <@corp.itstep>
2019-01-24 21:36:30 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkOnProperties/ArraySquareValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOnProperties
+{
+    class ArraySquareValue
+    {
+        private int[] array;
+
+        public ArraySquareValue(int sizeArray)
+        {
+            array = new int[sizeArray];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                value = (int) Math.Pow(value, 2);
+                array[index] = value;
+            }
+        }
+
+    }
+}
---
+++ HomeWorkOnProperties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOnProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int sizeArray;
+            sizeArray = int.Parse(Console.ReadLine());
+
+            ArraySquareValue array=new ArraySquareValue(sizeArray);
+
+            Random random = new Random();
+
+            int indexValue;
+            Console.Write("Входные значения: ");
+            for (int i = 0; i < sizeArray; i++)
+            {
+                indexValue= random.Next(1, 6);
+                Console.Write(indexValue+ " ");
+                array[i] = indexValue;
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Квадрат значений: ");
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
